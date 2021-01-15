a77a935cb8c8647e895a1128e162397be21dee3c
jjovanovic <jankic97@gmail.com>
2021-01-15 22:25:23 +0200

    Added routing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './property/nav-bar/nav-bar.component';
 import { HouseingService } from './services/houseing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+//Application routing
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,8 +22,9 @@ import { HouseingService } from './services/houseing.service';
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent,
+    AddPropertyComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [HouseingService],
   bootstrap: [AppComponent],
 })
